9dc54ee7a76a91816ec6e769efeaed487f6bac45
megan <msmeganmcmanus@gmail.com>
2021-02-22 19:25:00 -0800

    finish conceptual & addCommas

--- madlibs/src/addCommas.js
+++ madlibs/src/addCommas.js
@@ -1,3 +1,11 @@
-function addCommas() {}
+function addCommas(n) {	
+    if (n === parseFloat(n)) {
+    return parseFloat(n).toLocaleString();
+}
+if (n === parseInt(n)) {
+    return n.toLocaleString();
+}
+return NaN;
+}
 
 module.exports = addCommas;
